ecddfb154e140a371628a88a1fe21b4b199a0b8b
Kamil Machański <>
2018-04-30 16:35:38 +0200

    Stworzenie Tablicy zawierajacej figury

--- OOPL8/src/Main.java
+++ OOPL8/src/Main.java
@@ -1,4 +1,16 @@
-
+import FiguryPlaskie.Kolo;
+import FiguryPlaskie.Kwadrat;
+import FiguryPlaskie.Prostokat;
+import FiguryPlaskie.Romb;
+import FiguryPlaskie.Rownoleglobok;
+import FiguryPlaskie.Trapez;
+import FiguryPlaskie.Trojkat;
+import FiguryPrzestrzenne.Kula;
+import FiguryPrzestrzenne.Prostopadloscian;
+import FiguryPrzestrzenne.Stozek;
+import FiguryPrzestrzenne.Szescian;
+import FiguryPrzestrzenne.Walec;
+import KlasyAbstrakcyjne.FiguraGeometryczna;
 
 
 public class Main {
@@ -11,7 +23,7 @@ public class Main {
         
         Product[] tab = new Product[10];
         
-        tab[0] = new Ksiazka(21, "Mzżna", "tak");
+        tab[0] = new Ksiazka(21, "Można", "tak");
         tab[1] = new Czekolada(43, "taka sobie", "tak");
         tab[2] = new Ksiazka(32, "MOżna inaczej", "tak");
         tab[3] = new Dlugopis(3, "taż ok", "nie");
@@ -26,6 +38,35 @@ public class Main {
             
             tab[i].buy();
             tab[i].showInfo();
+        }
+        
+        System.out.println(" ");
+        System.out.println("-------------------------------------------------------");
+        System.out.println(" ");
+        
+        
+        FiguraGeometryczna[] figury = new FiguraGeometryczna[15];
+        
+        figury[0] = new Kwadrat(12);
+        figury[1] = new Walec(6, 12);
+        figury[2] = new Trapez(4, 8, 2, 15);
+        figury[3] = new Prostokat(4, 5);
+        figury[4] = new Szescian(25.8);
+        figury[5] = new Kula(30);
+        figury[6] = new Romb(9, 15);
+        figury[7] = new Kolo(33);
+        figury[8] = new Trojkat(45, 222.3);
+        figury[9] = new Prostopadloscian(3, 8, 4);
+        figury[10] = new Rownoleglobok(43, 65);
+        figury[11] = new Kwadrat(55);
+        figury[12] = new Szescian(66);
+        figury[13] = new Stozek(88, 69.9, 90);
+        figury[14] = new Romb(4, 4.3);
+        
+        int i=0;
+        while(i<figury.length){
+            System.out.println(figury[i].toString());
+            i++;
         }
         
         
